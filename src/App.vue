bb614a1ab8c3a8486e8c9de3754bf24ef5e60376
nicomanunta <nic.manun@gmail.com>
2024-03-01 15:02:35 +0100

    milestone 3 completata

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
+import ProjectCard from './components/ProjectCard.vue'
 export default {
-
+  components: {
+    ProjectCard
+  }
 }
 </script>
 <template lang="">
   <div>
-    
+    <ProjectCard/>
   </div>
 </template>
 <style lang="">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    data() {
+        return {
+            projects: [
+
+            ]
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <ul>
+            <li>Progetto1</li>
+        </ul>
+    </div>
+</template>
+<style lang="">
+    
+</style>
