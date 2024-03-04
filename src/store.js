94460443d902c6394fc322731546766cf2882dd9
nicomanunta <nic.manun@gmail.com>
2024-03-04 15:19:04 +0100

    aggiunto menu in store

--- src/store.js
+++ src/store.js
@@ -2,4 +2,14 @@ import { reactive } from 'vue';
 
 export const store = reactive({
     baseUrl: 'http://127.0.0.1:8000'
+    menu: [
+        {
+            name: 'home'
+            label: 'Home'
+        },
+        {
+            name: 'blog'
+            label: 'Blog'
+        }
+    ]
 })
