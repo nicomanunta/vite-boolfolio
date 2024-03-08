0305ad0250957f58af05d194e248f36540e090c8
nicomanunta <nic.manun@gmail.com>
2024-03-08 15:23:52 +0100

    ContactUs e ContactForm create

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,7 @@ export default {
         return {
             store,
         }
-    },
+    }, 
 }
 </script>
 <template lang="">
---
+++ src/pages/AppContactUs.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'AppContactUs'
+}
+</script>
+<template lang="">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Contatti</h1>
+                </div>
+                <div class="col-6">
+                    <h5>Città:</h5>
+                    <span>Cagliari (CA)</span>
+                    <h5>Indirizzo:</h5>
+                    <span>Viale Marconi 34</span>
+
+                </div>
+                <div class="col-6">
+                    <h5>Email:</h5>
+                    <span>pincopallino@gmail.com</span>
+                    <h5>Telefono:</h5>
+                    <span>+39 329123456789</span>  
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
